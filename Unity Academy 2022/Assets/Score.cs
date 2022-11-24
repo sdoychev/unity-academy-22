dcbb5c9ac25f6e42c2793b47d6de34ea63a1412d
Stefan Doychev <sdoychev@gmail.com>
2022-11-24 20:02:20 +0200

    Score updating

---
+++ Unity Academy 2022/Assets/Score.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    [SerializeField] private int magnitude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Score: " + score);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 up = collision.transform.up * magnitude;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(up);
+        Debug.Log("Score: " + ++score);
+    }
+
+}
